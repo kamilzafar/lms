d086f4e99692143814b265126668110ffabd0661
Syed Kamil Zafar <136997351+kamilzafar@users.noreply.github.com>
2026-01-02 18:27:48 +0500

    create logs

---
+++ lms/lms/doctype/lms_recording_access_log/lms_recording_access_log.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2026, Frappe and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+
+class LMSRecordingAccessLog(Document):
+	"""Log for tracking recording access - audit trail"""
+	pass
